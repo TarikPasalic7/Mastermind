805e46ea2a2ed9bf4ee78b7da02cea8df6a61fb3
unknown <tarik.pasalic777@gmail.com>
2021-07-10 15:07:39 +0200

    GameBoard component

---
+++ mastermind/src/components/Gameboard.js
@@ -0,0 +1,19 @@
+
+import React  from "react"
+const Gameboard =()=>{
+
+return(
+
+    
+ 
+    <div className="boardcontainer">
+    gameboard
+</div>
+
+)
+   
+
+
+}
+
+export default Gameboard;
