611e108cb207da86814c459491d7beafff405512
unknown <tarik.pasalic777@gmail.com>
2021-07-13 18:11:53 +0200

    check counter

--- mastermind/src/components/Board.js
+++ mastermind/src/components/Board.js
@@ -1,15 +1,28 @@
 
-import React,{useState}  from "react"
+import React,{useEffect,useState}  from "react"
 import Row from "./Row"
 
 const Board =({color,code})=>{
     const [usercode,setusercode]=[];
+   
     const [nextrow,setnextrow]=useState([{row1:true},{row2:false},{row3:false},{row4:false},{row5:false},
         {row6:false},{row7:false},{row8:false},{row9:false},{row10:false}]);
+  
+      const checkeduser=(arr)=>{
+         
 
-        const check =()=>{
+
+      }
+
+        const check =(usercode)=>{
+        let counter=0;
+        for (let index = 0; index < usercode.length; index++) {
+           if(usercode[index].iscolor==true)
+           counter++;
+            
+        }
+        console.log(counter);
         
-
        let arr=[...nextrow];
        arr[0].row1=false;
        arr[1].row2=true;
@@ -22,16 +35,9 @@ return(
     
  
     <div className="playcontainer">
-        <Row color={color} check={check}  isdisabled={nextrow[0].row1} />
-        <Row color={color} check={check}  isdisabled={nextrow[1].row2} />
-        <Row isdisabled={nextrow[2].row3}/>
-        <Row isdisabled={nextrow[3].row4}/>
-        <Row isdisabled={nextrow[4].row5}/>
-        <Row isdisabled={nextrow[5].row6}/>
-        <Row isdisabled={nextrow[6].row7}/>
-        <Row isdisabled={nextrow[7].row8}/>
-        <Row isdisabled={nextrow[8].row9}/>
-        <Row isdisabled={nextrow[9].row10}/>
+        <Row check={check} color={color}  isdisabled={nextrow[0].row1} />
+        <Row  check={check} color={color}   isdisabled={nextrow[1].row2} />
+    
         
       
              
--- mastermind/src/components/Row.js
+++ mastermind/src/components/Row.js
@@ -1,80 +1,79 @@
 
 import React,{useEffect,useState}  from "react"
-const Row =({isdisabled,color,check})=>{
-console.log(isdisabled);
-console.log(color)
-const [circle,setcircle]=useState([{iscolor:false,color:""},{iscolor:false,color:""},{iscolor:false,color:""},{iscolor:false,color:""}]);
-const addcolor=(e)=>{
-let cir=e.target.id;
-console.log(cir);
-if(cir==="circle1"){
+const Row =({isdisabled,check,color})=>{
+    const [circle,setcircle]=useState([{iscolor:false,color:""},{iscolor:false,color:""},{iscolor:false,color:""},{iscolor:false,color:""}]);
+    const addcolor=(e)=>{
+        let cir=e.target.id;
+        
+        if(cir==="circle1"){
+           
+            let newArr = [...circle]; 
+            newArr[0].iscolor=true; 
+            newArr[0].color=color; 
+            
+          
+        
+            setcircle(newArr);
+            
+        }
+        if(cir==="circle1"){
+           
+            let newArr = [...circle]; 
+            newArr[0].iscolor=true; 
+            newArr[0].color=color; 
+            
+          
+        
+            setcircle(newArr);
+            
+        }
+        if(cir==="circle2"){
+           
+            let newArr = [...circle]; 
+            newArr[1].iscolor=true; 
+            newArr[1].color=color; 
+            
+          
+        
+            setcircle(newArr);
+            
+        }
+        if(cir==="circle3"){
+           
+            let newArr = [...circle]; 
+            newArr[2].iscolor=true; 
+            newArr[2].color=color; 
+            
+          
+        
+            setcircle(newArr);
+            
+        }
+        if(cir==="circle4"){
+           
+            let newArr = [...circle]; 
+            newArr[3].iscolor=true; 
+            newArr[3].color=color; 
+            
+          
+        
+            setcircle(newArr);
+            
+        }
+        }
    
-    let newArr = [...circle]; 
-    newArr[0].iscolor=true; 
-    newArr[0].color=color; 
     
-  
 
-    setcircle(newArr);
-    
-}
-if(cir==="circle1"){
-   
-    let newArr = [...circle]; 
-    newArr[0].iscolor=true; 
-    newArr[0].color=color; 
-    
-  
-
-    setcircle(newArr);
-    
-}
-if(cir==="circle2"){
-   
-    let newArr = [...circle]; 
-    newArr[1].iscolor=true; 
-    newArr[1].color=color; 
-    
-  
-
-    setcircle(newArr);
-    
-}
-if(cir==="circle3"){
-   
-    let newArr = [...circle]; 
-    newArr[2].iscolor=true; 
-    newArr[2].color=color; 
-    
-  
-
-    setcircle(newArr);
-    
-}
-if(cir==="circle4"){
-   
-    let newArr = [...circle]; 
-    newArr[3].iscolor=true; 
-    newArr[3].color=color; 
-    
-  
-
-    setcircle(newArr);
-    
-}
-   
-    
-}
 return(
 
     
  
-    <button className="boardrow" disabled={isdisabled?false:true}>
-     {!circle[0].iscolor?<button onClick={addcolor} className="circle" id="circle1"  disabled={isdisabled?false:true}></button>:<button onClick={addcolor}  id="circle1" className={circle[0].color} disabled={isdisabled?false:true} ></button>}
+    <button className="boardrow" disabled={isdisabled?false:true} >
+   {!circle[0].iscolor?<button onClick={addcolor} className="circle" id="circle1"  disabled={isdisabled?false:true}></button>:<button onClick={addcolor}  id="circle1" className={circle[0].color} disabled={isdisabled?false:true} ></button>}
      {!circle[1].iscolor?<button onClick={addcolor} className="circle" id="circle2" disabled={isdisabled?false:true} ></button>:<button onClick={addcolor}  id="circle2" className={circle[1].color} disabled={isdisabled?false:true}></button>}
      {!circle[2].iscolor?<button onClick={addcolor} className="circle" id="circle3" disabled={isdisabled?false:true} ></button>:<button onClick={addcolor}  id="circle3" className={circle[2].color} disabled={isdisabled?false:true}></button>}
      {!circle[3].iscolor?<button onClick={addcolor} className="circle" id="circle4"  disabled={isdisabled?false:true}></button>:<button onClick={addcolor}  id="circle4" className={circle[3].color}disabled={isdisabled?false:true} ></button>}
-     <button onClick={check}>check</button>
+     <button onClick={()=>check(circle)}>check</button>
      <div  className="squarecontainer"> 
          <div className="square"></div>
          <div className="square"></div>
